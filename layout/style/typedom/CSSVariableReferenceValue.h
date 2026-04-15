82bb7f6855a3dcc434a568016fd5205d84d90c9b
Jan Varga <jvarga@igalia.com>
2026-04-15 10:30:10 +0000

    Bug 1990426 - Add missing pieces to CSSUnparsedValue for Typed OM reification; r=emilio,firefox-style-system-reviewers
    This patch adds the functionality required to support reification of custom
    properties involving unparsed values in CSS Typed OM.
    
    These additions are part of the initial infrastructure needed to expose
    unparsed values through the TypedValue enum and enable relevant WPT reification
    tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D293470

--- layout/style/typedom/CSSVariableReferenceValue.h
+++ layout/style/typedom/CSSVariableReferenceValue.h
@@ -59,6 +59,8 @@ class CSSVariableReferenceValue final : public nsISupports,
 
   // end of CSSVariableReferenceValue Web IDL declarations
 
+  const nsACString& GetVariable() const { return mVariable; };
+
  private:
   virtual ~CSSVariableReferenceValue() = default;
 
