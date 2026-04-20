3a122aef4366d823033cbac33ea53b088c71a0b5
Jon Coppeard <jcoppeard@mozilla.com>
2026-04-20 15:22:18 +0000

    Bug 2033378 - Pass typed data through telemetry callback API r=spidermonkey-reviewers,mgaudet
    This changes the telemetry callback to take a Variant of possible types rather
    than forcing everything to be represented as a 32 bit integer. This allows us
    to pass TimeDuration all the way down to Glean.
    
    Differential Revision: https://phabricator.services.mozilla.com/D295218

--- js/src/vm/Runtime.h
+++ js/src/vm/Runtime.h
@@ -216,46 +216,36 @@ class Metrics {
  public:
   explicit Metrics(JSRuntime* rt) : rt_(rt) {}
 
-  // Records a TimeDuration metric. These are converted to integers when being
-  // recorded so choose an appropriate scale. In the future these will be Glean
-  // Timing Distribution metrics.
-  struct TimeDuration_S {
+  // Records a TimeDuration metric.
+  struct TimeDuration {
     using SourceType = mozilla::TimeDuration;
-    static uint32_t convert(SourceType td) { return uint32_t(td.ToSeconds()); }
-  };
-  struct TimeDuration_MS {
-    using SourceType = mozilla::TimeDuration;
-    static uint32_t convert(SourceType td) {
-      return uint32_t(td.ToMilliseconds());
-    }
-  };
-  struct TimeDuration_US {
-    using SourceType = mozilla::TimeDuration;
-    static uint32_t convert(SourceType td) {
-      return uint32_t(td.ToMicroseconds());
+    static JSTelemetryData convert(SourceType td) {
+      return JSTelemetryData(td);
     }
   };
 
-  // Record a metric in bytes. In the future these will be Glean Memory
-  // Distribution metrics.
+  // Record a memory size metric.
   struct MemoryDistribution {
     using SourceType = size_t;
-    static uint32_t convert(SourceType sz) {
-      return static_cast<uint32_t>(std::min(sz, size_t(UINT32_MAX)));
+    static JSTelemetryData convert(SourceType sz) {
+      return JSTelemetryData(sz);
     }
   };
 
-  // Record a metric for a quanity of items. This doesn't currently have a Glean
-  // analogue and we avoid using MemoryDistribution directly to avoid confusion
-  // about units.
-  using QuantityDistribution = MemoryDistribution;
+  // Record a metric for a quanity of items.
+  struct QuantityDistribution {
+    using SourceType = size_t;
+    static JSTelemetryData convert(SourceType count) {
+      return JSTelemetryData(count);
+    }
+  };
 
   // Record the distribution of boolean values. In the future this will be a
   // Glean Rate metric.
   struct Boolean {
     using SourceType = bool;
-    static uint32_t convert(SourceType sample) {
-      return static_cast<uint32_t>(sample);
+    static JSTelemetryData convert(SourceType sample) {
+      return JSTelemetryData(sample);
     }
   };
 
@@ -264,9 +254,9 @@ class Metrics {
   // future, these should become Glean Labeled Counter metrics.
   struct Enumeration {
     using SourceType = unsigned int;
-    static uint32_t convert(SourceType sample) {
+    static JSTelemetryData convert(SourceType sample) {
       MOZ_ASSERT(sample <= 100);
-      return static_cast<uint32_t>(sample);
+      return JSTelemetryData(size_t(sample));
     }
   };
 
@@ -275,19 +265,21 @@ class Metrics {
   // Distribution unless they add a better match.
   struct Percentage {
     using SourceType = double;
-    static uint32_t convert(SourceType sample) {
+    static JSTelemetryData convert(SourceType sample) {
       MOZ_ASSERT(sample >= 0.0 && sample <= 100.0);
-      return static_cast<uint32_t>(sample);
+      return JSTelemetryData(size_t(sample));
     }
   };
 
   // Record an unsigned integer.
   struct Integer {
     using SourceType = uint32_t;
-    static uint32_t convert(SourceType sample) { return sample; }
+    static JSTelemetryData convert(SourceType sample) {
+      return JSTelemetryData(size_t(sample));
+    }
   };
 
-  inline void addTelemetry(JSMetric id, uint32_t sample);
+  inline void addTelemetry(JSMetric id, const JSTelemetryData& sample);
 
 #define DECLARE_METRIC_HELPER(NAME, TY)                \
   void NAME(TY::SourceType sample) {                   \
@@ -409,7 +401,7 @@ struct JSRuntime {
 
  public:
   // Accumulates data for Firefox telemetry.
-  void addTelemetry(JSMetric id, uint32_t sample);
+  void addTelemetry(JSMetric id, const JSTelemetryData& sample);
 
   void setTelemetryCallback(JSRuntime* rt,
                             JSAccumulateTelemetryDataCallback callback);
@@ -1130,7 +1122,7 @@ struct JSRuntime {
 
 namespace js {
 
-void Metrics::addTelemetry(JSMetric id, uint32_t sample) {
+void Metrics::addTelemetry(JSMetric id, const JSTelemetryData& sample) {
   rt_->addTelemetry(id, sample);
 }
 
