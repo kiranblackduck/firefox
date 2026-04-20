0179675ca5aa629359f532b18a280fd02eb79fce
Greg Stoll <gstoll@mozilla.com>
2026-04-20 00:44:48 +0000

    Bug 2029625 - turn on WebSerial pref by default r=haik,webidl,emilio,dom-worker-reviewers,edenchuang
    Differential Revision: https://phabricator.services.mozilla.com/D295057

--- dom/workers/test/test_worker_interfaces.js
+++ dom/workers/test/test_worker_interfaces.js
@@ -518,6 +518,10 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "Scheduler", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
+  { name: "Serial", android: false },
+  // IMPORTANT: Do not change this list without review from a DOM peer!
+  { name: "SerialPort", android: false },
+  // IMPORTANT: Do not change this list without review from a DOM peer!
   "ServiceWorker",
   // IMPORTANT: Do not change this list without review from a DOM peer!
   "ServiceWorkerContainer",
