28adf4c02d45ca8fb5b932b2922c8c8608afc645
Anna Yeddi ayeddi@mozilla.com <ayeddi@mozilla.com>
2026-04-20 19:42:53 +0000

    Bug 2030391 - Adding persistent accessible labels to Follow/Unfollow story buttons r=ayeddi,fluent-reviewers,home-newtab-reviewers,bolsson,nbarrett,thecount
    Differential Revision: https://phabricator.services.mozilla.com/D294028

--- browser/extensions/newtab/data/content/activity-stream.bundle.js
+++ browser/extensions/newtab/data/content/activity-stream.bundle.js
@@ -10579,7 +10579,8 @@ const CLOSE_ICON = "chrome://global/skin/icons/close.svg";
 function SectionFollowButton({
   following,
   onFollowClick,
-  onUnfollowClick
+  onUnfollowClick,
+  title
 }) {
   const [isHovered, setIsHovered] = (0,external_React_namespaceObject.useState)(false);
   const [justFollowed, setJustFollowed] = (0,external_React_namespaceObject.useState)(false);
@@ -10604,6 +10605,12 @@ function SectionFollowButton({
   } else if (following) {
     icon = CHECK_ICON;
   }
+
+  // Bug 2030391 - Provide an aria-label for the default icon state
+  const labelL10nId = following ? "newtab-section-unfollow-button-label" : "newtab-section-follow-button-label";
+  const labelL10nArgs = JSON.stringify({
+    topic: title
+  });
   const handleFollowClick = () => {
     setJustFollowed(true);
     onFollowClick();
@@ -10629,7 +10636,8 @@ function SectionFollowButton({
     type: buttonType,
     iconsrc: icon,
     onClick: following ? onUnfollowClick : handleFollowClick,
-    "data-l10n-id": isHovered ? followButtonL10nId : null
+    "data-l10n-id": isHovered ? followButtonL10nId : labelL10nId,
+    "data-l10n-args": isHovered ? null : labelL10nArgs
   }));
 }
 ;// CONCATENATED MODULE: ./content-src/components/DiscoveryStreamComponents/InterestPicker/InterestPicker.jsx
@@ -11699,7 +11707,8 @@ function CardSection({
   }, title), mayHaveSectionsPersonalization && novaEnabled && followable !== false && /*#__PURE__*/external_React_default().createElement(SectionFollowButton, {
     following: following,
     onFollowClick: onFollowClick,
-    onUnfollowClick: onUnfollowClick
+    onUnfollowClick: onUnfollowClick,
+    title: title
   }), subtitle && /*#__PURE__*/external_React_default().createElement("p", {
     className: "section-subtitle"
   }, subtitle)), mayHaveSectionsPersonalization && (novaEnabled ? /*#__PURE__*/external_React_default().createElement(SectionContextMenu, {
