95cdb37e670880c9bbe5c0c18d95ea608e6e08be
Tooru Fujisawa <arai_a@mac.com>
2026-04-24 16:20:15 +0000

    Bug 2031075 - Part 2: Clear unused stencil on memory pressure. r=bthrall
    Differential Revision: https://phabricator.services.mozilla.com/D294597

--- dom/script/ScriptLoader.cpp
+++ dom/script/ScriptLoader.cpp
@@ -3598,7 +3598,10 @@ void ScriptLoader::TryCacheRequest(ScriptLoadRequest* aRequest) {
   MOZ_ASSERT(mCache);
 
   if (mCache->IsLowMemory()) {
+    // If we're in the low-memory situation, do not create new cache entries.
+    // The stencil is no longer necessary after this point.
     TRACE_FOR_TEST(aRequest, "memorycache:memorypressure");
+    aRequest->ClearStencil();
     return;
   }
 
