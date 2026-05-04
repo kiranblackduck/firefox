72d7474c273c797de615f1cdc5d11d9d78f83ef8
Harshit Sohaney <hsohaney@mozilla.com>
2026-05-04 18:05:53 +0000

    Bug 2032602 - Update SharingUtils code to populate share menu on all platforms. r=fluent-reviewers,urlbar-reviewers,bolsson,Gijs,dao
    Differential Revision: https://phabricator.services.mozilla.com/D294999

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2922,9 +2922,7 @@ pref("browser.toolbars.bookmarks.showOtherBookmarks", true);
 // If preffed off, it makes it so that the button is no longer
 // available on the toolbar or in the customize section. Requires a
 // restart to reflect state changes.
-#ifndef XP_LINUX
-  pref("browser.toolbars.share-button.enabled", true);
-#endif
+pref("browser.toolbars.share-button.enabled", true);
 
 // Felt Privacy pref to control simplified private browsing UI
 pref("browser.privatebrowsing.felt-privacy-v1", false);
