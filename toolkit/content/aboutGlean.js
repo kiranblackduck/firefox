19b66786b08c564e72377cea5f9bc36c4adb20bc
Charlie Humphreys <chumphreys@mozilla.com>
2026-05-04 11:51:15 +0000

    Bug 2034107: Remove toggle for about:glean redesign and permanently enable it r=chutten,fluent-reviewers,bolsson
    Differential Revision: https://phabricator.services.mozilla.com/D297875

--- toolkit/content/aboutGlean.js
+++ toolkit/content/aboutGlean.js
@@ -14,7 +14,6 @@ const { AppConstants } = ChromeUtils.importESModule(
   "resource://gre/modules/AppConstants.sys.mjs"
 );
 
-let REDESIGN_ENABLED = false;
 const METRIC_DATA = {};
 let MAPPED_METRIC_DATA = [];
 let FILTERED_METRIC_DATA = [];
@@ -215,52 +214,39 @@ function showTab(button) {
 }
 
 function handleRedesign() {
-  REDESIGN_ENABLED = Services.prefs.getBoolPref("about.glean.redesign.enabled");
-  // If about:glean redesign is enabled, add the navigation category for it.
-  if (REDESIGN_ENABLED) {
-    const categories = document.getElementById("categories");
-    const div = document.createElement("div");
-    div.id = "category-metrics-table";
-    div.className = "category";
-    div.setAttribute("role", "menuitem");
-    div.setAttribute("tabindex", 0);
-    const span = document.createElement("span");
-    span.className = "category-name";
-    span.setAttribute("data-l10n-id", "about-glean-category-metrics-table");
-    div.appendChild(span);
-    categories.appendChild(div);
+  const categories = document.getElementById("categories");
+  const div = document.createElement("div");
+  div.id = "category-metrics-table";
+  div.className = "category";
+  div.setAttribute("role", "menuitem");
+  div.setAttribute("tabindex", 0);
+  const span = document.createElement("span");
+  span.className = "category-name";
+  span.setAttribute("data-l10n-id", "about-glean-category-metrics-table");
+  div.appendChild(span);
+  categories.appendChild(div);
 
-    document
-      .getElementById("enable-new-features")
-      .setAttribute("data-l10n-id", "about-glean-disable-new-features-button");
+  /**
+   * Handle metric filter input.
+   *
+   * This uses a timeout to debounce the events down to 200ms.
+   * Instead of updating the DOM every time the input changes, it'll only update when the input hasn't changed in the last 200ms since it last changed.
+   */
+  let inputTimeout = undefined;
+  document.getElementById("filter-metrics").addEventListener("input", e => {
+    clearTimeout(inputTimeout);
+    inputTimeout = setTimeout(() => {
+      updateFilteredMetricData(e.target.value.toLowerCase() ?? "");
+    }, 200);
+  });
 
-    /**
-     * Handle metric filter input.
-     *
-     * This uses a timeout to debounce the events down to 200ms.
-     * Instead of updating the DOM every time the input changes, it'll only update when the input hasn't changed in the last 200ms since it last changed.
-     */
-    let inputTimeout = undefined;
-    document.getElementById("filter-metrics").addEventListener("input", e => {
-      clearTimeout(inputTimeout);
-      inputTimeout = setTimeout(() => {
-        updateFilteredMetricData(e.target.value.toLowerCase() ?? "");
-      }, 200);
+  // Handle loading all metric data
+  document.getElementById("load-all").addEventListener("click", () => {
+    MAPPED_METRIC_DATA.forEach(datum => {
+      updateDatum(datum);
     });
-
-    // Handle loading all metric data
-    document.getElementById("load-all").addEventListener("click", () => {
-      MAPPED_METRIC_DATA.forEach(datum => {
-        updateDatum(datum);
-      });
-      updateTable();
-    });
-  } else {
-    document
-      .getElementById("enable-new-features")
-      .setAttribute("data-l10n-id", "about-glean-enable-new-features-button");
-    document.getElementById("category-metrics-table")?.remove();
-  }
+    updateTable();
+  });
 }
 
 function onLoad() {
@@ -315,17 +301,6 @@ function onLoad() {
   handleRedesign();
 
   DOCUMENT_BODY_SEL = d3.select(document.body);
-
-  document
-    .getElementById("enable-new-features")
-    .addEventListener("click", () => {
-      if (!REDESIGN_ENABLED) {
-        Services.prefs.setBoolPref("about.glean.redesign.enabled", true);
-      } else {
-        Services.prefs.setBoolPref("about.glean.redesign.enabled", false);
-      }
-      handleRedesign();
-    });
 
   document
     .getElementById("metrics-table-settings-button")
