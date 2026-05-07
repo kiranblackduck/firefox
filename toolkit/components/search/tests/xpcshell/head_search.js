705f53cc25fac844d4af9c3596b5dc249374a26b
Michael Kaply <345868+mkaply@users.noreply.github.com>
2026-05-07 19:30:18 +0000

    Bug 1995789 - Cleanup setupPolicyEngineWithJson. r=application-update-reviewers,search-reviewers,scunnane,jneuberger
    Differential Revision: https://phabricator.services.mozilla.com/D295633

--- toolkit/components/search/tests/xpcshell/head_search.js
+++ toolkit/components/search/tests/xpcshell/head_search.js
@@ -465,7 +465,7 @@ async function assertGleanDefaultEngine(expected) {
 }
 
 /**
- * Loads a new enterprise policy, and re-initialise the search service
+ * Loads a new enterprise policy, and re-initialises the search service
  * with the new policy. Also waits for the search service to write the settings
  * file to disk.
  *
@@ -473,15 +473,7 @@ async function assertGleanDefaultEngine(expected) {
  *   The enterprise policy to use.
  */
 async function setupPolicyEngineWithJson(policy) {
-  SearchService.reset();
-
-  await this.EnterprisePolicyTesting.setupPolicyEngineWithJson(policy);
-
-  let settingsWritten = SearchTestUtils.promiseSearchNotification(
-    "write-settings-to-disk-complete"
-  );
-  await SearchService.init();
-  await settingsWritten;
+  await this.EnterprisePolicyTesting.setupPolicyEngineWithJsonForSearch(policy);
 }
 
 /**
