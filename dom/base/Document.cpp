32a0ee5ccd767fb31207083f2fa005410cef05c8
Adam Vandolder <avandolder@mozilla.com>
2026-05-07 23:29:06 +0000

    Bug 2037457 - Don't exit pointerlock when applying fullscreen. r=dom-core,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D299150

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -16950,10 +16950,6 @@ bool Document::ApplyFullscreen(UniquePtr<FullscreenRequest> aRequest) {
   Document* fullScreenRootDoc =
       nsContentUtils::GetInProcessSubtreeRootDocument(this);
 
-  // If a document is already in fullscreen, then unlock the mouse pointer
-  // before setting a new document to fullscreen
-  PointerLockManager::Unlock("Document::ApplyFullscreen");
-
   // Set the fullscreen element. This sets the fullscreen style on the
   // element, and the fullscreen-ancestor styles on ancestors of the element
   // in this document.
